6cac0e9ef88a05aba0159d9fb45957c9e239a336
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:09:46 +0300

    Update 13-nested-loops.js

--- solutions/13-nested-loops.js
+++ solutions/13-nested-loops.js
@@ -1,5 +1,17 @@
-const _ = require("lodash"); 
-
-// BEGIN
-
-// END
+const _ = require("lodash");
+export default (arr1, arr2) => {
+  // Удаляем дубликаты из массивов с помощью lodash
+  const unique1 = _.uniq(arr1);
+  const unique2 = _.uniq(arr2);
+  let count = 0;
+  // Вложенные циклы для сравнения уникальных элементов
+  for (let i = 0; i < unique1.length; i++) {
+    for (let j = 0; j < unique2.length; j++) {
+      if (unique1[i] === unique2[j]) {
+        count++;
+        break; // Нашли совпадение, переходим к следующему элементу из первого массива
+      }
+    }
+  }
+  return count;
+};
