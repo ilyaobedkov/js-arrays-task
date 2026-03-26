eb6a50ec7cca01b313fa4bf3eaf0c7250b63abcd
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:07:14 +0300

    Update 8-removing.js

--- solutions/8-removing.js
+++ solutions/8-removing.js
@@ -1,3 +1,17 @@
-// BEGIN
-
-// END
+export default (array) => {
+  // Проверка на пустой массив
+  if (array.length === 0) {
+    return [];
+  }
+  const result = [];
+  // Определяем четность первого элемента
+  const firstElementParity = Math.abs(array[0]) % 2;
+  // Перебираем все элементы массива
+  for (const item of array) {
+    // Сравниваем четность текущего элемента с четностью первого
+    if (Math.abs(item) % 2 === firstElementParity) {
+      result.push(item);
+    }
+  }
+  return result;
+};
