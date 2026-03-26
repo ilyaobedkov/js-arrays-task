7b3178768a7017afd3fa95c765792452102bc9d9
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:06:10 +0300

    6-aggregation.js

--- solutions/6-aggregation.js
+++ solutions/6-aggregation.js
@@ -1,3 +1,15 @@
-// BEGIN
-
-// END
+export const calculateSum = (array) => {
+  // Проверка на пустой массив
+  if (array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  // Проходим по всем элементам массива
+  for (let i = 0; i < array.length; i++) {
+    // Проверяем, делится ли элемент на 3 без остатка
+    if (array[i] % 3 === 0) {
+      sum += array[i];
+    }
+  }
+  return sum;
+};
