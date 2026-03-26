5a69afa0aa9dd8807cb8ac368d8247293dbdc12e
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:06:49 +0300

    7-for-of.js

--- solutions/7-for-of.js
+++ solutions/7-for-of.js
@@ -1,3 +1,13 @@
-// BEGIN
-
-// END
+export default (temperatures) => {
+  // Проверка на пустой массив
+  if (temperatures.length === 0) {
+    return null;
+  }
+  let sum = 0;
+  // Используем цикл for...of для перебора элементов
+  for (const temperature of temperatures) {
+    sum += temperature;
+  }
+  // Вычисляем среднее арифметическое
+  return sum / temperatures.length;
+};
