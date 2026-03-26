c56b8fad02061fcfeeda45667a38a4ee6b497ace
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:12:14 +0300

    16-destructuring.js

--- solutions/16-destructuring.js
+++ solutions/16-destructuring.js
@@ -1,10 +1,31 @@
 const getDistance = ([x1, y1], [x2, y2]) => {
-    const xs = x2 - x1;
-    const ys = y2 - y1;
+  const xs = x2 - x1;
+  const ys = y2 - y1;
+
+  return Math.sqrt(xs ** 2 + ys ** 2);
+};
+
+// Реализация функции getTheNearestLocation
+export const getTheNearestLocation = (locations, point) => {
+  // Если мест нет, возвращаем null
+  if (locations.length === 0) {
+    return null;
+  }
   
-    return Math.sqrt(xs ** 2 + ys ** 2);
-  };
-
-// BEGIN
-
-// END
+  // Инициализируем первым местом
+  let nearestLocation = locations[0];
+  let shortestDistance = getDistance(point, nearestLocation[1]);
+  
+  // Перебираем остальные места с использованием деструктуризации
+  for (let i = 1; i < locations.length; i++) {
+    const [name, location] = locations[i];
+    const distance = getDistance(point, location);
+    
+    if (distance < shortestDistance) {
+      shortestDistance = distance;
+      nearestLocation = [name, location];
+    }
+  }
+  
+  return nearestLocation;
+};
