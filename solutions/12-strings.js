f36bbed58716523411ae7c8a4ce194d7bbd48468
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:09:12 +0300

    12-strings.js

--- solutions/12-strings.js
+++ solutions/12-strings.js
@@ -1,3 +1,11 @@
-// BEGIN
-
-// END
+export default (text, stopWords) => {
+  const words = text.split(' ');
+  const result = [];
+  
+  for (const word of words) {
+    // Используем тернарный оператор для более компактного кода
+    result.push(stopWords.includes(word) ? '$#%!' : word);
+  }
+  
+  return result.join(' ');
+};
