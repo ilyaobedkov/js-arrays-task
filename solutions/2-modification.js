1475297400bde23a4ee548c4ce0170a54e964e5d
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:03:39 +0300

    2-modification.js

--- solutions/2-modification.js
+++ solutions/2-modification.js
@@ -1,3 +1,13 @@
-// BEGIN
-
-// END
+export const swap = (array) => {
+  // Если массив содержит меньше двух элементов, возвращаем его без изменений
+  if (array.length < 2) {
+    return array;
+  }
+  
+  // Меняем местами первый и последний элементы
+  const firstElement = array[0];
+  array[0] = array[array.length - 1];
+  array[array.length - 1] = firstElement;
+  
+  return array;
+};
