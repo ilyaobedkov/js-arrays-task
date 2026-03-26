6ca9769c8eea0df59c2bb07f155a1806e3f505bd
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:05:44 +0300

    5-references.js

--- solutions/5-references.js
+++ solutions/5-references.js
@@ -1,3 +1,11 @@
-// BEGIN
-
-// END
+export const reverse = (array) => {
+  // Проходим только до середины массива
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    // Находим зеркальный индекс с конца
+    const mirrorIndex = array.length - 1 - i;
+    // Меняем местами элементы
+    const temp = array[i];
+    array[i] = array[mirrorIndex];
+    array[mirrorIndex] = temp;
+  }
+};
