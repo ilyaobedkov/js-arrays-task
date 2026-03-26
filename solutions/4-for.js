1c9927c23123fdaec8de2d1b0cd2b1a84bbdf4d7
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:05:05 +0300

    4-for.js

--- solutions/4-for.js
+++ solutions/4-for.js
@@ -1,3 +1,9 @@
-// BEGIN
-
-// END
+export default (array, prefix) => {
+  const result = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    result.push(`${prefix} ${array[i]}`);
+  }
+  
+  return result;
+};
