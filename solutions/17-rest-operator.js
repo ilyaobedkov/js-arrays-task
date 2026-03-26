ee5a2a489e4d2a99aefda2f0d9efe4cff1b858a5
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:12:33 +0300

    17-rest-operator.js

--- solutions/17-rest-operator.js
+++ solutions/17-rest-operator.js
@@ -1,3 +1,18 @@
-// BEGIN
-
-// END
+export const getMax = (array) => {
+  // Проверка на пустой массив
+  if (array.length === 0) {
+    return null;
+  }
+  // Используем деструктуризацию с rest-оператором
+  // first - первый элемент массива
+  // rest - все остальные элементы
+  const [first, ...rest] = array;
+  let max = first;
+  // Перебираем все остальные элементы
+  for (const item of rest) {
+    if (item > max) {
+      max = item;
+    }
+  }
+  return max;
+};
