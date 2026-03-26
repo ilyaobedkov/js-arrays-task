8f706f9a6583e83f133360a346b2274900eb8f4b
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:04:39 +0300

    3-isset.js

--- solutions/3-isset.js
+++ solutions/3-isset.js
@@ -1,3 +1,9 @@
-// BEGIN
-
-// END
+export const get = (array, index, defaultValue = null) => {
+  // Проверяем, существует ли элемент по указанному индексу
+  if (index >= 0 && index < array.length) {
+    return array[index];
+  }
+  
+  // Если индекс вне допустимого диапазона, возвращаем значение по умолчанию
+  return defaultValue;
+};
