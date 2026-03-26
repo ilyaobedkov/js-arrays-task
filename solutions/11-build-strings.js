0e9f41a555601519f7d3792ab3b487ecb45388e9
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-03-26 20:08:43 +0300

    11-build-strings.js

--- solutions/11-build-strings.js
+++ solutions/11-build-strings.js
@@ -1,3 +1,15 @@
-// BEGIN
-
-// END
+export default (definitions) => {
+  // Проверка на пустой массив
+  if (definitions.length === 0) {
+    return '';
+  }
+  let result = '<dl>';
+  // Перебираем все определения
+  for (const definition of definitions) {
+    const term = definition[0];
+    const description = definition[1];
+    result += `<dt>${term}</dt><dd>${description}</dd>`;
+  }
+  result += '</dl>';
+  return result;
+};
